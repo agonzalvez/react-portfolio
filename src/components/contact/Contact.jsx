bc5f630cdd1a574414e56554b3abebb2c7c4e8e8
Alejandro Gonzalvez <alejandrogonzalvez@Alejandros-MacBook-Pro.local>
2021-08-25 19:11:13 -0400

    Added link to github repos. Spelled out github and email link, and still kept the icons

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -12,25 +12,41 @@ export default function Contact() {
         </div>
       </div>
       <div className="right">
-      <a href="https://www.linkedin.com/in/gonzalvezalejandro/">
-      <i className="fab fa-linkedin fa-10x"></i>
-      </a>
-      <a href="mailto:agonzalvez1@gmail.com">
-      <i className="far fa-envelope fa-10x"></i>
-      </a>
-      <a href="https://github.com/agonzalvez">
-      <i class="fab fa-github fa-10x"></i>
-      </a>
-      <ul>
-        <li>
-          <h2>
-            Phone:
+        <a href="https://www.linkedin.com/in/gonzalvezalejandro/">
+          <i className="fab fa-linkedin fa-10x"></i>
+        </a>
+        <a href="mailto:agonzalvez1@gmail.com">
+          <i className="far fa-envelope fa-10x"></i>
+        </a>
+        <a href="https://github.com/agonzalvez">
+          <i class="fab fa-github fa-10x"></i>
+        </a>
+        <ul>
+          <li>
+            <h2>
+              Phone:
             </h2>
-        </li>
-        <li>
-        <a href="tel:+17863657652"><p>+1 (786) 365-7652</p></a>
-        </li>
-      </ul>
+          </li>
+          <li>
+            <a href="tel:+17863657652"><p>+1 (786) 365-7652</p></a>
+          </li>
+          <li>
+            <h2>
+              Github:
+            </h2>
+          </li>
+          <li>
+            <a href="https://github.com/agonzalvez"><p>github.com/agonzalvez</p></a>
+          </li>
+          <li>
+            <h2>
+              Email:
+            </h2>
+          </li>
+          <li>
+            <a href="mailto:agonzalvez1@gmail.com"><p>agonzalvez1@gmail.com</p></a>
+          </li>
+        </ul>
       </div>
     </div>
 
--- src/components/works/Works.jsx
+++ src/components/works/Works.jsx
@@ -6,55 +6,72 @@ export default function Works() {
   return (
     <div className="works" id="works">
 
-
-      <div className="work-1">
-        <div className="container">
-          <div className="item">
-            <h2>Feel Your Feelings</h2>
-            <p>Made with React.js</p>
-            <div className="right">
-            <a href="https://shrouded-lake-27009.herokuapp.com/" alt="Feel Your Feelings app"><img className="fyf-logo" /></a>
+        <div className="work-1">
+          <div className="container">
+            <div className="item">
+              <h2>Feel Your Feelings</h2>
+              <p>Made with React.js</p>
+              <div className="right">
+                <a href="https://shrouded-lake-27009.herokuapp.com/" alt="Feel Your Feelings app"><img className="fyf-logo" /></a>
+              </div>
             </div>
+            <ul>
+              <li><a href="https://shrouded-lake-27009.herokuapp.com/">Live App</a></li>
+              <li><a href="https://github.com/MarissaNancy/feel-your-feelings">Github Repo</a></li>
+            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="work-2">
-        <div className="container">
-          <div className="item">
-            <h2>ReadMe Generator</h2>
-            <p>Backend Generator using Inquirer</p>
-            <div className="right">
-            <a href="https://github.com/agonzalvez/readme-generator" alt="ReadMe Repository"><img className="read-me" /></a>
+        <div className="work-2">
+          <div className="container">
+            <div className="item">
+              <h2>ReadMe Generator</h2>
+              <p>Backend Generator using Inquirer</p>
+              <div className="right">
+                <a href="https://github.com/agonzalvez/readme-generator" alt="ReadMe Repository"><img className="read-me" /></a>
+              </div>
             </div>
+            <ul>
+              <li><a href="https://github.com/agonzalvez/readme-generator">Live App</a></li>
+              <li><a href="https://github.com/agonzalvez/readme-generator">Github Repo</a></li>
+            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="work-3">
-        <div className="container">
-          <div className="item">
-            <h2>Google Book Search</h2>
-            <p>Made with React, Axios and Mongoose</p>
-            <div className="right">
-            <a href="https://mysterious-meadow-63767.herokuapp.com/" alt="google book search app"><img className="google-book"/></a>
+        <div className="work-3">
+          <div className="container">
+            <div className="item">
+              <h2>Google Book Search</h2>
+              <p>Made with React, Axios and Mongoose</p>
+              <div className="right">
+                <a href="https://mysterious-meadow-63767.herokuapp.com/" alt="google book search app"><img className="google-book" /></a>
+              </div>
             </div>
+            <ul>
+              <li>
+                <a href="https://mysterious-meadow-63767.herokuapp.com/">Live App</a>
+                </li>
+              <li><a href="https://github.com/agonzalvez/google-booksearch">Github Repo</a></li>
+            </ul>
           </div>
         </div>
-      </div>
 
-      <div className="work-4">
-        <div className="container">
-          <div className="item">
-            <h2>NoSQL Fitness Tracker</h2>
-            <p>Made with React, Express, Mongoose and Morgan</p>
-            <div className="right">
-            <a href="https://floating-citadel-88773.herokuapp.com/" alt="fitness tracker app"><img className="fitness-tracker"/></a>
+        <div className="work-4">
+          <div className="container">
+            <div className="item">
+              <h2>NoSQL Fitness Tracker</h2>
+              <p>Made with React, Express, Mongoose and Morgan</p>
+              <div className="right">
+                <a href="https://floating-citadel-88773.herokuapp.com/" alt="fitness tracker app"><img className="fitness-tracker" /></a>
+              </div>
             </div>
+            <ul>
+              <li><a href="https://floating-citadel-88773.herokuapp.com/">Live App</a></li>
+              <li><a href="https://github.com/agonzalvez/nosql-fitness-tracker">Github Repo</a></li>
+            </ul>
           </div>
         </div>
-      </div>
-
+      
     </div>
 
 
